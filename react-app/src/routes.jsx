97a253b717e0ef9cb4cd38314d13ab749e2327d3
abyss-s <xyz@inha.edu>
2024-10-31 18:39:08 +0900

    feat(Layout.jsx): Layout 페이지 구현

--- react-app/src/routes.jsx
+++ react-app/src/routes.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import Layout from "./components/Layout";
+import Layout from "./pages/Layout";
 import Example from "./components/Example";
 
 const router = createBrowserRouter([
