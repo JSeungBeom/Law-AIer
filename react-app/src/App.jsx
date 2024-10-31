4b0b67e2e68d7646850642aadd54b26a87615060
정재현 <chwogus0303@naver.com>
2024-10-31 19:04:58 +0900

    feat(react-app): 메인페이지에 등록

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Payment from './components/Payment';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -44,6 +45,7 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        <Payment />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
