d4d3870a7016ca30ab45f5dfd60a3ae28b8a4904
정재현 <chwogus0303@naver.com>
2024-10-31 16:10:55 +0900

    feat(springboot-app): Social Login 로그인 API 구현
    Naver, Google, Kakao 추가

---
+++ springboot-app/src/main/java/com/inha/springbootapp/domain/oauth/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.inha.springbootapp.domain.oauth.controller;
+
+import com.inha.springbootapp.domain.oauth.service.AuthService;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import io.github.cdimascio.dotenv.Dotenv;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+    Dotenv dotenv = Dotenv.load();
+
+    @GetMapping("/callback")
+    public void callback(@RequestParam String loginType ,@RequestParam String code, HttpServletResponse response) throws IOException {
+        String accessToken = authService.callback(loginType, code, response);
+        String redirectUrl = dotenv.get("SOCIAL_LOGIN_REDIRECT_URL") + "?accessToken=" + accessToken;
+        response.sendRedirect(redirectUrl);
+    }
+}
