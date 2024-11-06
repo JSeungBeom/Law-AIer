78d8cfab3366cc0015a814afb826f803c5120f2a
abyss-s <xyz@inha.edu>
2024-11-07 02:22:15 +0900

    feat(routes.jsx): Login 페이지 라우터 연결

--- react-app/src/routes.jsx
+++ react-app/src/routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Layout from './pages/Layout';
 import Example from './components/Example';
+import Login from './pages/Login';
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
       {
         path: 'ex',
         element: <Example />,
+      },
+      {
+        path: 'login',
+        element: <Login />,
       },
     ],
   },
