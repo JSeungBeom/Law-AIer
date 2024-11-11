e79a9b4e191b20441aff033ead52fa512ec5a881
Jang Seung Beom <wkdtmf357@gmail.com>
2024-11-12 01:12:40 +0900

    feat: TermInfo 레포지토리 추가

--- springboot-app/src/main/java/com/inha/springbootapp/domain/terminfo/repository/TermInfoRepository.java
+++ springboot-app/src/main/java/com/inha/springbootapp/domain/terminfo/repository/TermInfoRepository.java
@@ -1,4 +1,33 @@
-package com.inha.springbootapp.domain.terminfo;
+package com.inha.springbootapp.domain.terminfo.repository;
 
+import com.inha.springbootapp.domain.terminfo.entity.TermInfo;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
 public class TermInfoRepository {
+
+    private final EntityManager em;
+
+    public void save(TermInfo termInfo){
+        em.persist(termInfo);
+    }
+
+    public List<TermInfo> findAll(){
+        return em.createQuery("select t from TermInfo t", TermInfo.class).getResultList();
+    }
+
+    public TermInfo findOne(Long id){
+        return em.find(TermInfo.class, id);
+    }
+
+    public List<TermInfo> findByName(String name){
+        return em.createQuery("select t from TermInfo t where t.termName = :name", TermInfo.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
 }
