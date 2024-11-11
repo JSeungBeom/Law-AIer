aa6c31b08b6c29a5bdb12795398c1ea39e478eaa
abyss-s <xyz@inha.edu>
2024-11-11 16:58:51 +0900

    ✨ Feat(PriceList.jsx): 구독권 안내 페이지 구현
    구독권을 만들기 위한 Price 컴포넌트를 활용

---
+++ react-app/src/pages/PriceList.jsx
@@ -0,0 +1,30 @@
+import Price from '../components/Payment/Price';
+import Button from '../components/Button/Button';
+import { useNavigate } from 'react-router-dom';
+
+const features = [
+  '토큰 제한 없는 챗봇 사용',
+  '광고 제거',
+  '전문 법조인과의 상담 연결',
+];
+
+const PriceList = () => {
+  const navigate = useNavigate();
+
+  const handleNavigatePayment = () => {
+    navigate('/payment');
+  };
+
+  return (
+    <Price
+      title="프리미엄 플랜"
+      audience="For Individual Users"
+      price="₩3,900/월"
+      features={features}
+      ButtonComponent={
+        <Button onClick={handleNavigatePayment} label="지금 바로 결제하기" />
+      }
+    />
+  );
+};
+export default PriceList;
