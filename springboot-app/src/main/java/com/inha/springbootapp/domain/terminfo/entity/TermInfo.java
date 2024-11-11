b587cec56d651eb8eceec7cf6398109c96a4fe88
Jang Seung Beom <wkdtmf357@gmail.com>
2024-11-12 01:12:11 +0900

    feat: TermInfo 엔티티 추가

--- springboot-app/src/main/java/com/inha/springbootapp/domain/terminfo/entity/TermInfo.java
+++ springboot-app/src/main/java/com/inha/springbootapp/domain/terminfo/entity/TermInfo.java
@@ -1,4 +1,25 @@
 package com.inha.springbootapp.domain.terminfo.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import lombok.Getter;
+
+@Entity
+@Getter
 public class TermInfo {
+
+    @Id
+    @Column(name = "TERM_ID")
+    private Long id;
+
+    private String termName;
+
+    @Lob
+    private String source;
+
+    @Lob
+    private String description;
+
 }
