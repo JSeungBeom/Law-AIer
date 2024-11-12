866604b3d5887fdd031cbef265b610f5795d7ae7
Jang Seung Beom <wkdtmf357@gmail.com>
2024-11-13 03:00:10 +0900

    update: 동기화를 위한 경로 변경

--- springboot-app/src/main/java/com/inha/springbootapp/global/GlobalCorsConfig.java
+++ springboot-app/src/main/java/com/inha/springbootapp/global/GlobalCorsConfig.java
@@ -1,4 +1,4 @@
-package com.inha.springbootapp;
+package com.inha.springbootapp.global;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
