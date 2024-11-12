c59853cc2f853507b333e696f77940e5a64ffea1
Jang Seung Beom <wkdtmf357@gmail.com>
2024-11-13 03:00:43 +0900

    feat: TermDetail DTO 추가

--- springboot-app/src/main/java/com/inha/springbootapp/domain/terminfo/dto/TermDetailDto.java
+++ springboot-app/src/main/java/com/inha/springbootapp/domain/terminfo/dto/TermDetailDto.java
@@ -1,4 +1,21 @@
 package com.inha.springbootapp.domain.terminfo.dto;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class TermDetailDto {
+    private Long id;
+
+    private String termName;
+
+    private String source;
+
+    private String description;
 }
